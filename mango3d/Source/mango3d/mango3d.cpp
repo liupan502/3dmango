3140018791e767650d8d486628d90760da536ee5
liupan <liupan@123.com>
2016-10-16 10:39:24 +0800

    add project files

---
+++ mango3d/Source/mango3d/mango3d.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "mango3d.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, mango3d, "mango3d" );
---
+++ mango3d/Source/mango3d/mango3dGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "mango3dGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MANGO3D_API Amango3dGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
